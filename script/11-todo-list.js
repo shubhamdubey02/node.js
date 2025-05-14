bd87e858c793a35e0708e3d34f9d6e16676f6577
Shubham dubey <shubham.dubey@oodles.io>
2025-05-14 12:11:18 +0530

    loop start

---
+++ script/11-todo-list.js
@@ -0,0 +1,12 @@
+const todolist = [];
+
+function addTodo() {
+    const inputElement = document.querySelector('.js-name-input');
+    const name = inputElement.value;
+    console.log(name);
+
+    todolist.push(name);
+    console.log(todolist);
+
+    inputElement.value = '';
+}
